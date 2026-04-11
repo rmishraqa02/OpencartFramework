ec5168e3098ae6c1c7aef5b082c2013b13ea7c6a
RajeshMishraChiku <rajeshmishra.qa02@gmail.com>
2026-04-11 20:02:54 +0530

    updated cart page as per review comments

--- src/main/java/com/opencart/qa/pages/CartPage.java
+++ src/main/java/com/opencart/qa/pages/CartPage.java
@@ -1,10 +1,13 @@
 package com.opencart.qa.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
-	int qty = 10;
+	By qty = By.id("cart");
 	
 	public void addToCart() {
 		System.out.println(qty);
+		System.out.println("Cart is updated");
 	}
 }
