f1ddfecf57e1c496e1c8a2a21151b0bf05453257
rmishraqa02 <chandnipanda2137@gmail.com>
2026-04-20 06:52:30 +0530

    Create NewPage.java

---
+++ src/main/java/com/opencart/qa/pages/NewPage.java
@@ -0,0 +1,11 @@
+package com.opencart.qa.pages;
+
+public class NewPage {
+	
+public static void main(String[] args) {
+	    System.out.println("Hello New Page");
+	 int x =10;
+	 System.out.println(x );
+  String name = "Naveen";
+}
+}
